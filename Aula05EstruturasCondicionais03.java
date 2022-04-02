ffbf4fdb50cea7c4a6681595e3c1c9f2d47be39a
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2022-04-02 19:44:22 -0300

    AulaJavaMaratona0503

---
+++ Aula05EstruturasCondicionais03.java
@@ -0,0 +1,10 @@
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario = 6000;
+
+        //(condição) ? verdadeiro : falso
+        String resultado = salario > 5000 ? "Eu vou doar 500" : "Não tenho contições";
+
+        System.out.printf(resultado);
+    }
+}
